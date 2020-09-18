60638c407b3d80cdf3492e8e00ac834d9280cf0b
Yiğit Çeviker <taskma@gmail.com>
2020-09-18 22:09:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Esp32 &amp; Sonoff Basic Home Automation
+![alt text](https://github.com/taskma/Esp32-sonoff_Home-_Automation/blob/master/sonoff.jpeg)
+![alt text](https://github.com/taskma/Esp32-sonoff_Home-_Automation/blob/master/esp8266.jpeg)
+
+
---
+++ config.h
@@ -0,0 +1,24 @@
+// Your Wifi SSID.
+#define WIFI_SSID "wifi"
+
+// Wifi password.
+#define WIFI_PASSWORD ""
+
+// MQTT server details.
+#define MQTT_ADDRESS "192.168.0.1"
+#define MQTT_USERNAME ""
+#define MQTT_PASSWORD ""
+
+//Select Which device is uploading
+#define hostNumber        0
+
+//Fill All Devices Name
+String deviceNames[] = {
+ "sonoff_saloon1",              // 0 ==> 
+ "sonoff_saloon2",       // 1 ==> 
+ "sonoff_kitchen1",  // 2 ==> 
+ "sonoff_kitchen2",           // 3 ==> 
+ "sonoff_living_room1",          // 4 ==> 
+ "sonoff_living_room2",      // 5 ==>
+//
+};
